cbc0ed044e16e85082604539fee1dab595fbab07
VITO\BYLF <frederik.byl@vito.be>
2025-03-10 13:31:28 +0100

    Backend runs at port 3000

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,13 +1,8 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
 
-// This file will be prefilled with variables available in the environment.
-// A "prestart" hook is in place to do so. See /preinstall.
 export const environment = {
   production: false,
-  frontendUrl: undefined,
-  backendUrl: undefined,
-  amaUrl: undefined,
-  amaConsentPath: undefined
+  frontendUrl: "http://localhost:4200",
+  backendUrl: "http://localhost:3000",
+  amaUrl: "https://we-are-acc.vito.be",
+  amaConsentPath: "/en/access-request"
 };
